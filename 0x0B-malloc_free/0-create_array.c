8b7794f9f666d4304a66a3bf877d0062863fda9d
Ahmed <oladapoahmed5@gmail.com>
2023-10-05 23:25:46 +0100

    Added a logical expression at the if statement

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	strings = malloc(size * sizeof(char));
-	if (strings == NULL)
+	if (strings == NULL || strings == 0)
 	{
 		return (NULL);
 	}
