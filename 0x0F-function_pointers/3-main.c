8b5f5a1150f30b19c656b323f565699898cbb78e
Ahmed <oladapoahmed5@gmail.com>
2023-10-13 17:56:44 +0100

    Finished writing the 5 source code for the simple calculator task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+/**
+ * main - 
+ * @argc: number of arguments
+ * @argv: array of arguements
+ * Return: Always 0 means Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char operator;
+	int (*cal)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	cal =  get_op_func(argv[2]);
+
+	if (!cal)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operator = *argv[2];
+
+	if ((operator == '/' || operator == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = cal(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - The sum of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: The sum int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - The difference between two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: The difference int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - The product of two int
+ * @a: first integer
+ * @b: second integer
+ * Return: The product int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two int
+ * @a: first integer
+ * @b: second integer
+ * Return: The result int when divided
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Finds the reamainder when divided
+ * @a: first integer
+ * @b: second integer
+ * Return: The remainder int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
