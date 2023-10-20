c25d39d732955d4eb5072f7787b67a57b85e8ccc
Ahmed <oladapoahmed5@gmail.com>
2023-10-20 05:39:59 +0100

    Fixed a typo error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@
 void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house back!\n");
+	printf(",\nI bore my house upon my back!\n");
 }
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race)");
+	return (0);
+}
